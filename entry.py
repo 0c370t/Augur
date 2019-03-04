7c50ea6030a39926e6eaa869be718e30fbde67d6
Brian Donald <contact@noimbrian.com>
2019-03-03 21:31:11 -0600

    Attempt 0
    Created debug function that should respond with the file posted, assuming file was posted as formData. Currently non-functional, commiting to share for debugging

--- entry.py
+++ entry.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2.7
-from flask import Flask, Blueprint, render_template, jsonify, request
-
+from flask import Flask, Blueprint, render_template, jsonify, request, Response, url_for, send_file
+from tempfile import NamedTemporaryFile
+from shutil import copyfileobj
 # Used if augur is to be a Blueprint
 # augur = Blueprint("augur", __name__,
 #                    template_folder = "templates",
@@ -15,3 +16,22 @@ augur.secret_key = "EFF121E88B54D79A39CCF18E358BB"
 def index():
     # Displays an explanation of the API
     return render_template("index.html.jinja")
+
+
+@augur.route("/debug/formData", methods=["POST"])
+def debug():
+    # Current intended functionality is returning the image that is sent as form data
+    if 'file' not in request.files:
+        return str(request.files) + "\n" + str(request.args) + "\n"
+    image = getImageFromRequest(request)
+    image_name = image.filename
+    return send_file(image, attachment_filename=image_name, as_attachment=True)
+
+
+def getImageFromRequest(request):
+    # This method will be expanded to include other ways of including a file, rather than just including it in the initial request (such as a url to get the image from)
+    # Ensure file actually exists
+    if 'file' not in request.files:
+        raise Exception("No File in given request!")
+    image = request.files['file']
+    return image
