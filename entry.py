9051f36ac3ccd33ca5084c2bd74867a36919907f
Brian Donald <contact@noimbrian.com>
2019-03-04 19:07:24 -0600

    Removed silliness
    Augur will not be used as a blueprint.

--- entry.py
+++ entry.py
@@ -6,11 +6,6 @@ import json
 import sys
 import os
 
-# Used if augur is to be a Blueprint
-# augur = Blueprint("augur", __name__,
-#                    template_folder = "templates",
-#                    static_folder = "static")
-
 augur = Flask(__name__, static_url_path="", static_folder="static")
 application = augur
 augur.secret_key = "EFF121E88B54D79A39CCF18E358BB"
