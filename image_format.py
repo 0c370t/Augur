d59ffecfc2c3c2dbd5ff1f5f3681f4593cbbee9f
Brian Donald <contact@noimbrian.com>
2019-03-07 15:52:55 -0600

    Redefined valid_formats
    I was so focused on whether or not I could, that I didn't stop think about whether I should. (Flexed Python's list comprehension)

--- image_format.py
+++ image_format.py
@@ -22,10 +22,7 @@ format_extension_map = {
 }
 
 valid_formats = format_extension_map.keys()
-valid_extensions = []
-for format in format_extension_map:
-    for extension in format_extension_map[format]:
-        valid_extensions += [extension]
+valid_extensions = [extension for format in valid_formats for extension in format_extension_map[format]]
 
 def getFormatByExtension(extension):
     extension = extension.split('.')[-1].lower()
